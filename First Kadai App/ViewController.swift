e179de09e43418c9fcaff8a435c4da702d341f5f
karin.sasaki <sasakikarin15@gmail.com>
2021-03-17 01:22:50 +0900

    Initial Commit

---
+++ First Kadai App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First Kadai App
+//
+//  Created by 佐々木かりん on 2021/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
